f625041f212d3d5cb70a9d9e788a42018a6c120a
Ssa45 <the45@mail.ru>
2020-04-26 16:40:18 +0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const pug = require('gulp-pug');
+
+/* ---------------Server---------------- */
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
+    });
+
+    gulp.watch('build/**/*').on('change', browserSync.reload);
+});
+
+/* ---------------Pug complete---------------- */
+gulp.task('templates:compile', function buildHTML() {
+    return gulp.src('views/*.pug')
+    .pipe(pug({
+      // Your options in here.
+    }))
+  });
